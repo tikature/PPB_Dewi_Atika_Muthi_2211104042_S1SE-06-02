e87edb3184afadf3d616af247a76409978d5fd0f
tikature <detikaa10@gmail.com>
2024-11-13 23:21:59 +0700

    UP GUIDED 9

---
+++ 09_API_Perangkat_Keras/GUIDED/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:md7/image_picker_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ImagePickerScreen(type: ImageSourceType.gallery),
+    );
+  }
+}
